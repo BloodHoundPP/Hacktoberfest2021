58d7bcaacd42e3c90de3387fd3d694da51ce2e18
BloodHoundPP <75355146+BloodHoundPP@users.noreply.github.com>
2021-10-22 23:27:16 +0530

    Add files via upload

---
+++ HotelList.js
@@ -0,0 +1,33 @@
+import hotel1 from "../images/hotel-1.jpg"
+import hotel2 from '../images/hotel-2.jpg'
+import hotel3 from '../images/hotel-3.jpg'
+import hotel4 from '../images/hotel-4.jpg'
+
+const HotelList = [
+    {
+        id : "1",
+        Image : hotel1,
+        title : "Divya Abhilasha",
+        text : "north indian , maharashtrian"
+    },
+    {
+        id : "2",
+        Image: hotel2,
+        title : "Bhole’s Sandwitch center",
+        text : "Snacks"
+    },
+    {
+        id : "3",
+        Image : hotel3,
+        title : "Rock’n Roll",
+        text : "Snacks"
+    },
+    {
+        id : "4",
+        Image: hotel4,
+        title : "Bon Vivant",
+        text : "Indian cuisines"
+    },
+]
+
+export default HotelList
